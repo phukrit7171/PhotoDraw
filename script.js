5aabeb5c00bac069b809446f22493652eaffd900
Phukrit Kittinontana <phukrit7171@gmail.com>
2023-12-07 12:02:16 +0700

    add randomWalk

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
 // script.js
 
-// Function to be called when the "Random" button is clicked
-function randomPic() {
+// Function to simulate random walk and draw points on the canvas
+function randomWalk() {
     // Get the canvas element and its context
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
-  
-    // Calculate the canvas size to maintain a 4:3 aspect ratio
+
+    // Calculate the canvas size to maintain a 16:9 aspect ratio
     var containerWidth = canvas.parentElement.offsetWidth;
-    var canvasWidth = containerWidth * 0.9; // 90% of the container width
-    var canvasHeight = (canvasWidth * 9) / 16; // Maintain a 4:3 aspect ratio
-  
+    var canvasWidth = containerWidth * 0.8; // 80% of the container width
+    var canvasHeight = (canvasWidth * 9) / 16; // Maintain a 16:9 aspect ratio
+
     // Set the canvas size
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
-  
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-    // Generate random coordinates for the black dot within the canvas
-    var randomX = Math.floor(Math.random() * canvas.width);
-    var randomY = Math.floor(Math.random() * canvas.height);
-  
-    // Set the fill color to black
-    ctx.fillStyle = "black";
-  
-    // Draw a black dot at the random coordinates
-    ctx.beginPath();
-    ctx.arc(randomX, randomY, 3, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-  
-  // Make sure to call the randomPic function when the page is loaded
-  document.addEventListener("DOMContentLoaded", function () {
-    randomPic();
-  });
-  
-  // Update the canvas size on window resize
-  window.addEventListener("resize", function () {
-    randomPic();
-  });
-  
+
+    // Starting position
+    var x = canvas.width / 2;
+    var y = canvas.height / 2;
+
+    var xx = x;
+    var yy = y;
+
+    var walk = 50000;
+
+    // Simulate random walk
+    for (var i = 0; i < walk; i++) {
+        var r = Math.random() * 360;
+        xx += Math.cos((r * Math.PI) / 180);
+        yy += Math.sin((r * Math.PI) / 180);
+
+        // Draw a point
+        ctx.beginPath();
+        ctx.arc(xx, yy, 1, 0, 2 * Math.PI);
+        ctx.fillStyle = "black";
+        ctx.fill();
+    }
+}
+// Make sure to call the randomWalk function when the page is loaded
+document.addEventListener("DOMContentLoaded", function () {
+    randomWalk();
+});
+
+// Update the canvas size on window resize
+window.addEventListener("resize", function () {
+    randomWalk();
+});
