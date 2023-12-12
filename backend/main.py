2523f5aadecb5630b7e085ec9f75d88ed8fc6fac
Phukrit Kittinontana <phukrit7171@gmail.com>
2023-12-12 14:02:24 +0700

    err 422

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,23 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from pathlib import Path
 
 app = FastAPI()
 
-@app.post("/upload-image")
+@app.post("/upload")
+async def upload_image(file: UploadFile = File(...,content_type=["image/jpg","image/jpeg"])):
+    # Process the uploaded image
+    contents = await file.read()
+
+    # Define the directory to save images (create it if it doesn't exist)
+    save_directory = Path("Problem")
+    save_directory.mkdir(parents=True, exist_ok=True)
+
+    # Create a path for the saved image using the original filename
+    save_path = save_directory / file.filename
+
+    # Save the image to the specified directory
+    with save_path.open("wb") as image_file:
+        image_file.write(contents)
+
+    return JSONResponse(content={"message": "Image uploaded and saved successfully", "filename": file.filename})
--- frontend/script.js
+++ frontend/script.js
@@ -42,17 +42,40 @@ function randomWalk() {
 function uploadImage() {
     const fileInput = document.getElementById('imgUpload');
     const uploadedImage = document.getElementById('uploadedImage');
-    
+
     const file = fileInput.files[0];
 
     if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        uploadedImage.src = e.target.result;
-      };
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            uploadedImage.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
 
-      reader.readAsDataURL(file);
+        sendImg_toServer(file);
     } else {
-      console.error('No file selected');
+        console.error('No file selected');
     }
-  }
+}
+function sendImg_toServer(file) {
+    // Create a new FormData object to handle the file data
+    const formData = new FormData();
+
+    // Instead of appending the base64 string, append the raw file data
+    formData.append('image', file, file.name);
+
+    // Perform a fetch request to the backend endpoint (replace with your actual backend URL)
+    fetch('http://localhost:8000/upload', {
+        method: 'POST',
+        body: formData,
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Log the response from the backend
+            console.log('Response from backend:', data);
+        })
+        .catch(error => {
+            // Log an error message
+            console.error('Error uploading image:', error);
+        });
+}
